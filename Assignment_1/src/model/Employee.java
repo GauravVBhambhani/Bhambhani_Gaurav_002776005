4a83fb19fba9f6794a65a4981b4ef09cd0421bd5
Gaurav Bhambhani <bhambhani.g@northeastern.edu>
2022-09-29 15:51:54 -0400

    Created UI for Create Employee

---
+++ Assignment_1/src/model/Employee.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author gorubhambhani
+ */
+public class Employee {
+    
+    private String empName;
+    private Integer empId;
+    private Integer empAge;
+    private String empGender;
+    private String empJoinDate;
+    private String empLevel;
+    private String empPosTitle;
+    private String empContactInfo;
+    private String empImage;
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public Integer getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
+    }
+
+    public Integer getEmpAge() {
+        return empAge;
+    }
+
+    public void setEmpAge(Integer empAge) {
+        this.empAge = empAge;
+    }
+
+    public String getEmpGender() {
+        return empGender;
+    }
+
+    public void setEmpGender(String empGender) {
+        this.empGender = empGender;
+    }
+
+    public String getEmpJoinDate() {
+        return empJoinDate;
+    }
+
+    public void setEmpJoinDate(String empJoinDate) {
+        this.empJoinDate = empJoinDate;
+    }
+
+    public String getEmpLevel() {
+        return empLevel;
+    }
+
+    public void setEmpLevel(String empLevel) {
+        this.empLevel = empLevel;
+    }
+
+    public String getEmpPosTitle() {
+        return empPosTitle;
+    }
+
+    public void setEmpPosTitle(String empPosTitle) {
+        this.empPosTitle = empPosTitle;
+    }
+
+    public String getEmpContactInfo() {
+        return empContactInfo;
+    }
+
+    public void setEmpContactInfo(String empContactInfo) {
+        this.empContactInfo = empContactInfo;
+    }
+
+    public String getEmpImage() {
+        return empImage;
+    }
+
+    public void setEmpImage(String empImage) {
+        this.empImage = empImage;
+    }
+}
---
+++ Assignment_1/src/model/EmployeeDirectory.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gorubhambhani
+ */
+public class EmployeeDirectory {
+    
+    private ArrayList<Employee> database;
+    
+    public EmployeeDirectory(){
+        database = new ArrayList<Employee>();
+    }
+
+    public ArrayList<Employee> getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(ArrayList<Employee> database) {
+        this.database = database;
+    }
+
+}
